f4901ea6a5a16a7736dab08aae812e59b17a41a1
yl <78755@qq.com>
2018-11-08 00:05:21 +0800

    style

--- js/admin_index.js
+++ js/admin_index.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
   $(".item").click(function(){
     if($(this).next("ul").hasClass("active")){
-      $(this).next("ul").removeClass("active")
+      $(this).next("ul").removeClass("active");
+      $(this).find("i").last().css("transform","rotate(0deg)")
     }else{
       $(this).next("ul").addClass('active');
+      $(this).find("i").last().css("transform","rotate(180deg)")
     }
   });
+  var tbody=document.getElementById("table_body");
+  var btn1=tbody.querySelectorAll("tr>td>button:first-child");
+  console.log(btn1);
 });
 
